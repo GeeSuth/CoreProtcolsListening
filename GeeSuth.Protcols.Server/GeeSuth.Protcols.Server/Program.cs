de97d32aeaea161180ac7dfee385d1791fef94f3
GeeSuth <servecemodea@gmail.com>
2021-09-28 20:22:58 +0300

    init

---
+++ GeeSuth.Protcols.Server/GeeSuth.Protcols.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeeSuth.Protcols.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
